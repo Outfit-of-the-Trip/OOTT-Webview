5a0fdee4c755455066d911f790f2fb0880fcb958
aestera <aest3ra@gmail.com>
2023-08-25 03:06:53 +0900

    feat : 지도 center와 가까이에 있는 마커만 표시

--- src/components/PhotoZone/kakaoMap.js
+++ src/components/PhotoZone/kakaoMap.js
@@ -1,11 +1,38 @@
-import React, { useRef, useState } from 'react';
-import { CustomOverlayMap, Map, MapMarker, ZoomControl } from 'react-kakao-maps-sdk';
-import axios from 'axios';
+import React, { useState } from 'react';
+import { Map, MapMarker, ZoomControl } from 'react-kakao-maps-sdk';
 import locations from './location'
 
 const { kakao } = window;
 
 const KakaoMap = () => {
+
+  function degreesToRadians(degrees) {
+    return degrees * (Math.PI / 180);
+  }
+  
+  function calcDistance(loc) {
+    const earthRadiusKm = 6371; // 지구 반지름 (단위: km)
+    var lat1 = loc.latlng.lat
+    var lat2 = state.center.lat 
+    var lon1 = loc.latlng.lng
+    var lon2 = state.center.lng
+  
+    const dLat = degreesToRadians(lat2 - lat1);
+    const dLon = degreesToRadians(lon2 - lon1);
+  
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(degreesToRadians(lat1)) *
+        Math.cos(degreesToRadians(lat2)) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
+  
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  
+    const distance = earthRadiusKm * c; // 두 지점 간의 거리 (단위: km)
+    if(distance > 10) return false;
+    else return true;
+  }
 
   
   const [state, setState] = useState({
@@ -30,13 +57,15 @@ const KakaoMap = () => {
           style={{ width: '100vw', height: '100vh'  }} 
           level={4}
       > 
+
       {locations.map((loc, idx) => (
+        (calcDistance(loc)) && (
         <MapMarker
             key={`${loc.title}-${loc.latlng.lat}`}
             position={loc.latlng}
             image={{
-              src: "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png",
-              size: { width: 64, height: 69 },
+              src: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FkLHA6%2Fbtsr9RBWrGm%2FKh2kmCuC37g5a3rgMvasK0%2Fimg.png",
+              size: { width: 30, height: 40 },
             }}
             clickable={true}
 
@@ -47,8 +76,7 @@ const KakaoMap = () => {
                 isPanto: true,
               })
             } }
-        >
-        </MapMarker>
+        />)
       ))}
       <ZoomControl position={kakao.maps.ControlPosition.TOPRIGHT} />
 
